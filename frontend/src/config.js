a0a4187e1362a4589b5a8f7e18909b1d2d430c40
Shivp55 <shivparekh803@gmail.com>
2025-11-25 21:22:32 +0000

    Fix GitHub Pages config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export default defineConfig({
-    plugins: [react()],
-    base: "/shaishav-portfolio/", // your repo name
-});
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import { HashRouter } from "react-router-dom";
-
-root.render(
-    <HashRouter>
-        <App />
-    </HashRouter>
-);
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
