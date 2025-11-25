dfb11e81e4aeb5de8700e090c3a943ad9d865173
Shivp55 <shivparekh803@gmail.com>
2025-11-25 23:13:20 +0000

    deploy_fixed_routing_updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/shaishav_portfolio/", // repo name
+  base: "/shaishav-portfolio/", // repo name
 });
